ac7bc68607cb7ba0875dc080b113520860508b47
Nate Martin <thenatemartin@gmail.com>
2022-12-27 16:47:35 -0500

    Added in support for multiple masses/ edges. Still requires hardcoding

--- ControlSim_Rust/src/main.rs
+++ ControlSim_Rust/src/main.rs
@@ -18,15 +18,68 @@ fn main() {
 
 
     let mut dots:Vec<simulation::Dot> = Vec::new();
+    let mut edges:Vec<simulation::Edge> = Vec::new();
+
     dots.push(simulation::Dot{
-        position:(300.0, 400.0),
+        position:(400.0, 400.0),
         velocity:(0.0, 0.0),
+        force:(0.0, 0.0),
+        fixed: true,
+        mass: 1.0,
         id:0,
     });
 
-    for ind in 1..100000 {
-        simulation::step(& mut dots);
-        if ind % 100 == 0 {
+    dots.push(simulation::Dot{
+        position:(300.0, 400.0),
+        velocity:(0.0, 0.0),
+        fixed: false,
+        force:(0.0, 0.0),
+        mass: 4.0,
+        id:1,
+    });
+
+    dots.push(simulation::Dot{
+        position:(300.0, 300.0),
+        velocity:(0.0, 0.0),
+        fixed: false,
+        force:(0.0, 0.0),
+        mass: 1.0,
+        id:2,
+    });
+
+    dots.push(simulation::Dot{
+        position:(200.0, 300.0),
+        velocity:(0.0, 0.0),
+        fixed: false,
+        force:(0.0, 0.0),
+        mass: 1.0,
+        id:3,
+    });
+
+    edges.push(simulation::Edge{
+        id:0,
+        connections:(0, 1),
+        stiffness:100000.0,
+        len: 100.0,
+    });
+
+    edges.push(simulation::Edge{
+        id:1,
+        connections:(1, 2),
+        stiffness:100000.0,
+        len: 100.0,
+    });
+
+    edges.push(simulation::Edge{
+        id:1,
+        connections:(2, 3),
+        stiffness:10.0,
+        len: 100.0,
+    });
+
+    for ind in 1..100_000_000 {
+        simulation::step(& mut dots, &edges);
+        if ind % 10000 == 0 {
             write_dots(&dots, & mut file);
         }
     };
@@ -38,7 +91,7 @@ fn main() {
 
 fn write_dots(dots: &Vec<simulation::Dot>, file: & mut File) {
 
-    match write!(file, "({:.2}, {:.2})\n", dots[0].position.0, dots[0].position.1) {
+    match write!(file, "({:.2}, {:.2}), ({:.2}, {:.2}), ({:.2}, {:.2})\n", dots[1].position.0, dots[1].position.1, dots[2].position.0, dots[2].position.1, dots[3].position.0, dots[3].position.1) {
         Err(_) => panic!("Couldn't write"),
         Ok(file) => file,
     };
--- ControlSim_Rust/src/simulation.rs
+++ ControlSim_Rust/src/simulation.rs
@@ -1,7 +1,19 @@
+
+
 pub struct Dot {
-    pub id: i32,
+    pub id: usize,
     pub position: (f32, f32),
     pub velocity: (f32, f32),
+    pub force: (f32, f32),
+    pub mass: f32,
+    pub fixed: bool,
+}
+
+pub struct Edge {
+    pub id: usize,
+    pub connections: (usize, usize),
+    pub stiffness: f32,
+    pub len: f32,
 }
 
 
@@ -12,41 +24,74 @@ Ok aso uh wahts the plan here then?
 I THINK that I'm going to have the first position hardcoded and then I'll just calculate where the pendulum ends up
 then I can print that out and bobs your uncle everythings fine and dandy like a hard candy christmas!
 
+*/
+pub fn step(dots: & mut Vec<Dot>, edges: & Vec<Edge>) {
 
+    let t_step = 0.000001; // sec/ Frame
+    let g = 500.0; // (Pixels/ sec^2)
+
+    /*
+        General Flow: 
+        iterate through each dot and zero out the total force (to neutral with gravity)
+        iterate through each edge and add on the force to said edge
+        iterate through each dot and apply these changes
+    */
+
+    // Resets the force on each dot so that the edges can be applied
+    for dot in &mut *dots {
+        dot.force = (0.0, dot.mass * g);
+        
+    };
+
+    // Sums the rest of the forces that are applied to each mass
+    for edge in edges {
+        let d1:& Dot = & dots[edge.connections.0];
+        let d2:& Dot = & dots[edge.connections.1];
+        if d1.id != edge.connections.0 || d2.id != edge.connections.1 {
+            panic!("Dots are not being stored at the proper position!");
+        }
+        let x_delt:f32 = d1.position.0 - d2.position.0;
+        let y_delt:f32 = d1.position.1 - d2.position.1;
+       
+        let curr_len:f32 = ( x_delt.powi(2) + y_delt.powi(2) ).powf(0.5);
+        let total_force:f32 = (curr_len - edge.len) * edge.stiffness;
+
+        
+
+       /* Notes for implementation below:
+        * x_delt > 0 when d1 is further to the right. Force on d1 is NEGATIVE when x_delt is positive.
+        *   Because it should be pulled to the left
+        * y_delt > 0 when d1 is further down. Force on d1 is NEGATIVE when y_delt is positive.
+        * This is because a negative force is an upwards force, as per the implementation of screen coordinates.
+        */
+
+        let d1:&mut Dot = &mut dots[edge.connections.0];
+        d1.force.0 -= x_delt / curr_len * total_force;
+        d1.force.1 -= y_delt / curr_len * total_force;
+        
+
+        let d2:&mut Dot = &mut dots[edge.connections.1];
+        d2.force.0 += x_delt / curr_len * total_force;
+        d2.force.1 += y_delt / curr_len * total_force;
+    };
+
+    // Calculates the new positions and velocities for the masses
+    for dot in dots {
+        
+        if ! dot.fixed { 
+            dot.velocity.0 += dot.force.0 / dot.mass * t_step;
+            dot.velocity.1 += dot.force.1 / dot.mass * t_step;
+            dot.position.0 += dot.velocity.0 * t_step;
+            dot.position.1 += dot.velocity.1 * t_step;
+        }
+    };
+}
+
+/*
+
+Ok SO, this is going to parse through my grammar literally just so that I can prove that I'm a badass
 
 */
-pub fn step(dots: & mut Vec<Dot>) {
-    /*
-        I'll comment this all way better after i get a proof of concept working, but first I'm going to hack together a single pendulum deal
-    */
-
-    // Note to self, this vector will currently only have a single entry in it, but ill make it better one day!
-
-    let t_step = 0.0001; // sec/ Frame
-    let g = 500.0; // (Pixels/ sec^2)
-    let rad = 100.0; // Pixels (rod length)
-    let stiff = 10.0; // Force/pixel (rod stiffness)
-    let mass = 1.0; // mass
-    /*
-        Ok so im about to go way too hard and determine the actualy fricken properties of a rod. This is going to suck but will be ok
-    */
-
-    let grav_force = mass * g; // be positive 
-    // call tension positive then this wrong
-    let rod_force = stiff * (((400.0 - dots[0].position.0).powi(2) + (400.0 - dots[0].position.1).powi(2)).powf(0.5) - rad);
-    let rod_force_x = rod_force * (400.0 - dots[0].position.0)/rad; //be positive
-    let rod_force_y = rod_force * (400.0 - dots[0].position.1)/rad; // be negative
-    // First, we will solve for the new acceleration
-    // this points downwards and needs to be covered by the centripital
-
-    // Add the acceleration to the position
-    let this_dot = & mut dots[0];
-    this_dot.velocity.0 += rod_force_x / mass * t_step;
-    this_dot.velocity.1 += ( grav_force + rod_force_y) / mass * t_step;
-    //println!("Rod Force Y: {}", rod_force_y);
-
-    this_dot.position.0 += this_dot.velocity.0 * t_step;
-    this_dot.position.1 += this_dot.velocity.1 * t_step;
-    //println!("Pos is {:?}", this_dot.position);
-    // Repeat?
+pub fn read_scene() -> (Vec<Dot>, Vec<Edge>) {
+    
 }
